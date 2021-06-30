17e3883f1aba3260095f541daae7890865e111e6
Andrei Stan <mlgsanic0@gmail.com>
2021-06-30 21:48:15 +0300

    fixed dockspace name typo, fixed resizing issue

--- new_main.cpp
+++ new_main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "ImGuiFileBrowser.h"
 #include "image_loader.h"
+#include "imgui_internal.h"
 imgui_addons::ImGuiFileBrowser file_dialog; // As a class member or globally
 
 bool show_dialog(std::string &filepath)
@@ -56,7 +57,6 @@ int main()
     GLuint my_image_texture = 0;
     layer.init_imgui();
     ImGuiIO &io = ImGui::GetIO();
-    (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
@@ -74,7 +74,12 @@ int main()
     ImGui_ImplSDL2_InitForOpenGL(layer.get_window(), layer.get_gl_context());
     ImGui_ImplOpenGL3_Init(glsl_version.c_str());
 
-    ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     bool done = false;
     while (!done)
@@ -89,21 +94,64 @@ int main()
                 done = true;
         }
         layer.on_new_frame();
-
         int my_image_width, my_image_height;
 
+        //            DOCKSPACE START
+        ImGuiID dockspace_id;
+        const ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        window_flags |= ImGuiWindowFlags_NoTitleBar ;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+        ImGui::Begin("Dockspace", nullptr, window_flags);
+
+        ImGui::PopStyleVar(3);
+
+        if (ImGui::DockBuilderGetNode(ImGui::GetID("MyDockSpace")) == NULL)
+        {
+            dockspace_id = ImGui::GetID("MyDockSpace");
+
+            ImGui::DockBuilderRemoveNode(dockspace_id);               
+            ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+
+            ImGuiID dock_main_id = dockspace_id; 
+            ImGuiID dock_id_right = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Right, 0.2f, NULL, &dock_main_id);
+            ImGuiID dock_id_bottom = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Down, 0.2f, NULL, &dock_main_id);
+
+            ImGui::DockBuilderDockWindow("Logs", dock_id_bottom);
+            ImGui::DockBuilderDockWindow("Filters", dock_id_right);
+            ImGui::DockBuilderDockWindow("Image Viewer", dock_main_id);
+            ImGui::DockBuilderFinish(dockspace_id);
+        }
+        dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        ImGui::End();
+
+        ImGui::Begin("Logs", nullptr, ImGuiWindowFlags_NoTitleBar);
+        ImGui::End();
+
+        ImGui::Begin("Filters", nullptr, ImGuiWindowFlags_NoTitleBar);
+        ImGui::End();
+
+        ImGui::Begin("Image Viewer", nullptr,  ImGuiWindowFlags_NoTitleBar);
         if (show_dialog(filename))
         {
             std::cout << "show img " << std::endl;
             std::cout << filename << std::endl;
-            if(LayerLoadTextureFromFile(filename.c_str(), &my_image_texture, &my_image_width, &my_image_height))
+            if (LayerLoadTextureFromFile(filename.c_str(), &my_image_texture, &my_image_width, &my_image_height))
             {
                 std::cout << "suc" << std::endl;
             }
         }
 
-        ImGui::Begin("Opencv");
-        
         ImGui::Image((void *)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
 
         ImGui::End();
